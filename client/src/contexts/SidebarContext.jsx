d822b6e87a1be2a77d6f36ee58924d2f5748b806
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-21 23:31:49 +0000

    tidying up

--- client/src/contexts/SidebarContext.jsx
+++ client/src/contexts/SidebarContext.jsx
@@ -1,21 +1,21 @@
 import React, { createContext, useState } from 'react';
 
 export const SidebarContext = createContext({
-  sidebar: true,
-  toggleSidebar: () => {},
+    sidebar: true,
+    toggleSidebar: () => { },
 });
 
 export const SidebarProvider = ({ children }) => {
-  const [sidebar, setSidebar] = useState(true);
+    const [sidebar, setSidebar] = useState(true);
 
-  const toggleSidebar = () => {
-    setSidebar(!sidebar);
-  };
+    const toggleSidebar = () => {
+        setSidebar(!sidebar);
+    };
 
-  return (
-    <SidebarContext.Provider value={{ sidebar, toggleSidebar }}>
-      {children}
-    </SidebarContext.Provider>
-  );
+    return (
+        <SidebarContext.Provider value={{ sidebar, toggleSidebar }}>
+            {children}
+        </SidebarContext.Provider>
+    );
 };
 
