e8195fbb796a53328ad9a9da5abcd4e7e6f3315f
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-21 14:49:07 +0000

    added logout button

--- client/src/components/NavbarD/index.jsx
+++ client/src/components/NavbarD/index.jsx
@@ -1,13 +1,45 @@
 import Wrapper from "../../assets/wrappers/Navbar"
-import {FaHome} from 'react-icons/fa'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { useState } from 'react';
 
 const NavbarD = () => {
     return (
-        <Wrapper>
-            <h2>Navbar</h2>
-            <FaHome/>
-        </Wrapper>
-    )
-}
-
-export default NavbarD
+      <Wrapper>
+        <div className='nav-center'>
+          <button
+            type='button'
+            className='toggle-btn'
+            onClick={() => console.log('toggle sidebar')}
+          >
+            <FaAlignLeft />
+          </button>
+          <div>
+            <h3 className='logo-text'>dashboard</h3>
+          </div>
+          <div className='btn-container'>
+            <button
+              type='button'
+              className='btn'
+              onClick={() => console.log('toggle logout dropdown')}
+            >
+              <FaUserCircle />
+              <FaCaretDown />
+            </button>
+            <div className='dropdown show-dropdown'>
+              <button
+                type='button'
+                className='dropdown-btn'
+                onClick={() => {
+                  console.log('logout user');
+                }}
+              >
+                logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+    );
+  };
+  
+  export default NavbarD;
