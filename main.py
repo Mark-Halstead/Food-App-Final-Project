eb980ea913b19c1901798693f493bdd39e0777b4
sadik312 <sadikislam46@gmail.com>
2023-03-21 12:34:17 +0000

    simple chat room setup

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from app import create_app, socketio
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
-app = create_app(debug=True)
+app = Flask(__name__)
+app.config['SECRET'] = 'secret!123'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+def handle_message(message):
+    print("Received message: " + message)
+    if message != "User connected!":
+        send(message, broadcast=True)
+    
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="localhost")
