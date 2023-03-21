52590668ca22be60a5e68db299e659576b859aad
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-03-21 18:40:57 +0000

    Add python_version pipfile

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,12 +1,11 @@
-export {default as Navbar} from './Navbar'
-export {default as NavLink} from './NavLink'
-export {default as NavLinks} from './NavLinks'
-export {default as Landing} from './Landing'
-export {default as LandingLogo} from './LandingLogo'
-export {default as PricingCards} from './PricingCards'
-export {default as Hamburger} from './Hamburger'
-export {default as NavbarD} from './NavbarD'
-export {default as BigSideBar} from './BigSideBar'
-export {default as SmallSideBar} from './SmallSideBar'
+export { default as Navbar } from "./Navbar";
+export { default as NavLink } from "./NavLink";
+export { default as NavLinks } from "./NavLinks";
+export { default as Landing } from "./Landing";
+export { default as LandingLogo } from "./LandingLogo";
+export { default as PricingCards } from "./PricingCards";
+export { default as Hamburger } from "./Hamburger";
+export { default as NavbarD } from "./NavbarD";
+export { default as BigSideBar } from "./BigSideBar";
+export { default as SmallSideBar } from "./SmallSideBar";
 // export {default as Wrapper} from './Wrapper'
-
