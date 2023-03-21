793a929a358e3005b0f241a555f1561095a882cd
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-21 16:15:21 +0000

    started getting sidebar working

--- client/src/components/SmallSideBar/index.jsx
+++ client/src/components/SmallSideBar/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Wrapper from "../../assets/wrappers/SmallSidebar"
+import { FaTimes } from 'react-icons/fa';
 
 const SmallSideBar = () => {
     return (
         <Wrapper>
-            <h2>SmallSideBar</h2>
+            <div className="sidebar-container show-sidebar" >
+                <div className="content" >
+                    <button className='close-btn' onClick={() => console.log('toggle')} >
+                        <FaTimes/>
+                    </button>
+                    <header>
+                        {/* Our Team logo here */}
+                    </header>
+                    <div className='nav-links'>nav links</div>
+                </div>
+            </div>
         </Wrapper>
     )
 }
