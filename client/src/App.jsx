7a5dff00853979fd1225416c594d0d837cf34102
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-21 12:12:19 +0000

    added some routes for different parts of dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { HomePage, Dashboard } from './pages'
 import { Routes, Route } from 'react-router-dom'
 import { Navbar } from './components'
+import { Profile, Overview, Plan, SharedLayout, Chat } from './pages/Dashboard'
 
 
 function App() {
@@ -9,11 +10,16 @@ function App() {
   return (
     <>
       <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route index element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<HomePage />} />
+        </Route>
+        <Route path="/dashboard" element={<SharedLayout />}>
+          <Route path='overview' element={<Overview />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='plan' element={<Plan />} />
+          <Route path='chat' element={<Chat />} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ client/src/pages/Dashboard/index.js
@@ -0,0 +1,7 @@
+import Chat from "./Chat";
+import Overview from "./Overview";
+import Plan from "./Plan";
+import Profile from "./Profile";
+import SharedLayout from "./SharedLayout"
+
+export { Chat, Overview, Plan, Profile, SharedLayout }
