74a2b030094085e90fa47ba3bf4898f164372c74
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-03-21 13:18:53 +0000

    Add User model and /users routes

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
+from pymongo import MongoClient
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+from app.routes import user_routes
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
-# db setup
+@app.route('/')
+def index():
+    return {"welcome":"Welcome to the PlatePal API"}
+
+
+app.register_blueprint(user_routes, url_prefix="/users")
