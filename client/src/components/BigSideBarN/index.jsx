2e6fd90776ac92b236de8dba92ca6640ac04d6a9
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-23 09:44:10 +0000

    a

--- client/src/components/BigSideBarN/index.jsx
+++ client/src/components/BigSideBarN/index.jsx
@@ -1,29 +1,29 @@
-// import React, { useContext } from 'react'
-// import Wrapper from "../../assets/wrappers/BigSideBar"
-// import DashNavLinksN from '../DashNavLinksN';
-// import { SidebarContext } from '../../contexts/SidebarContext';
+import React, { useContext } from 'react'
+import Wrapper from "../../assets/wrappers/BigSideBar"
+import DashNavLinksN from '../DashNavLinksN';
+import { SidebarContext } from '../../contexts/SidebarContext';
 
-// const BigSideBarN = () => {
+const BigSideBarN = () => {
 
-//     const { sidebar } = useContext(SidebarContext);
+    const { sidebar } = useContext(SidebarContext);
 
-//     return (
-//         <Wrapper>
-//             <div
-//                 className={
-//                     !sidebar
-//                         ? 'sidebar-container'
-//                         : 'sidebar-container show-sidebar'
-//                 }
-//             >
-//                 <div className='content'>
-//                     <header>
-//                     </header>
-//                     <DashNavLinksN />
-//                 </div>
-//             </div>
-//         </Wrapper>
-//     );
-// }
+    return (
+        <Wrapper>
+            <div
+                className={
+                    !sidebar
+                        ? 'sidebar-container'
+                        : 'sidebar-container show-sidebar'
+                }
+            >
+                <div className='content'>
+                    <header>
+                    </header>
+                    <DashNavLinksN />
+                </div>
+            </div>
+        </Wrapper>
+    );
+}
 
-// export default BigSideBarN
+export default BigSideBarN
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,14 +2,14 @@ export { default as Navbar } from "./Navbar";
 export { default as NavLink } from "./NavLink";
 export { default as NavLinks } from "./NavLinks";
 export { default as Landing } from "./Landing";
-// export { default as LandingLogo } from "./LandingLogo";
+export { default as LandingLogo } from "./LandingLogo";
 export { default as PricingCards } from "./PricingCards";
 export { default as Hamburger } from "./Hamburger";
 export { default as NavbarD } from "./NavbarD";
-// export { default as BigSideBar } from "./BigSideBar";
+export { default as BigSideBar } from "./BigSideBar";
 export { default as SmallSideBar } from "./SmallSideBar";
 export { default as ChartContainer } from "./ChartContainer";
 export { default as StatsContainer } from "./StatsContainer";
 export { default as DashNavLinksN } from "./DashNavLinksN";
-// export { default as BigSideBarN } from "./BigSideBarN";
+export { default as BigSideBarN } from "./BigSideBarN";
 
