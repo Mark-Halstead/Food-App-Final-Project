d528f5365a444c3a00a5bd8d80c56a8c75a32bd0
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-28 09:55:27 +0100

    a user can update their preferences

--- client/src/pages/Dashboard/Profile.jsx
+++ client/src/pages/Dashboard/Profile.jsx
@@ -1,105 +1,21 @@
 import React, { useState } from 'react';
-import { FormFormat } from "../../components"
+import { useNavigate } from 'react-router-dom';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 
 const Profile = () => {
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  };
 
-  const handleChange = (e) => {
-    e.preventDefault()
-  };
+  const Navigate = useNavigate()
+
+  const handleClick = () => {
+    Navigate("/user-signup-form")
+  }
 
   return (
     <Wrapper>
-      <form className='form' onSubmit={handleSubmit}>
-        <h3>profile</h3>
-        <div className='form-center'>
-          <FormFormat
-            type='text'
-            name='Activity Level'
-            value='High'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Goal'
-            value='Goal'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Dietary Restrictions'
-            value="None"
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Allergies'
-            value='None'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Food Preferences'
-            value='None'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type="text"
-            name='Meal Frequency'
-            value='low'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Meal Complexity'
-            value='Complex'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Budget'
-            value='£1000000'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Allergies'
-            value='other'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Food Preferences'
-            value='other'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Meal Complexity'
-            value='other'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Budget'
-            value='other'
-            handleChange={handleChange}
-          />
-          <FormFormat
-            type='text'
-            name='Subscription Type'
-            value='other'
-            handleChange={handleChange}
-          />
-          <button type='submit' className='btn btn-block'>
-            Next 
-          </button>
-        </div>
-      </form>
+      <h3>Update Preferences</h3>
+      <button className='btn btn-block' onClick={() => handleClick()}>
+        Update Preferences
+      </button>
     </Wrapper>
   );
 };
