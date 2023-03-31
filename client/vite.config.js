8ac5fdb4cda8646afb7cf51501f57ffbfaebe9ca
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-31 13:01:10 +0100

    a

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './'
-})
+  base: '/roulette-simulation/'
+});
