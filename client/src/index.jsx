a9afd7e08f8d86e49bcbfd659ae761c7e944fc42
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-03-31 09:37:47 +0100

    a

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { SidebarProvider } from './contexts/SidebarContext';
