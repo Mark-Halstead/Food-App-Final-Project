599b84adee0852aa87b7f18128d1915f7223f7fe
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-03-31 09:24:57 +0100

    got rid of auth0

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import { Auth0Provider } from '@auth0/auth0-react'
 import { SidebarProvider } from './contexts/SidebarContext';
 import 'normalize.css'
 import './dashboard.css'
@@ -12,18 +11,10 @@ import './dashboard.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-114ylhztictuc35u.eu.auth0.com"
-      clientId="C1LUd5GgB7TPtsRQWc7Faf9afGk7PZtj"
-      authorizationParams={{
-        redirect_uri: `${window.location.origin}/dashboard`
-      }}
-    >
       <Router>
         <SidebarProvider>
           <App />
         </SidebarProvider>
       </Router>
-    </Auth0Provider>
   </React.StrictMode>,
 )
